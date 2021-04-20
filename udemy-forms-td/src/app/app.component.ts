c5973e6af93be5fd42171ae6c8dbced53713fc2b
Viktor <malyshkin.viktor@icloud.com>
2021-04-20 19:32:52 +0300

    s15 p198. TD: Setting and Patching Form Values

--- udemy-forms-td/src/app/app.component.ts
+++ udemy-forms-td/src/app/app.component.ts
@@ -15,6 +15,16 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser'
+    // this.signupForm.setValue({
+    //   userDate: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   select: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male',
+    // })
+    this.signupForm.form.patchValue({userData: {username: suggestedName }})
   }
 
   // onSubmit(form: NgForm) {
