68a29c5df6b5788e2ecc74736808d014c7d4f7b8
Viktor <malyshkin.viktor@icloud.com>
2021-04-20 21:57:54 +0300

    finish

--- udemy-forms-td-assignment/src/app/app.component.ts
+++ udemy-forms-td-assignment/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core'
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('formEx', {static: false}) signupForm: NgForm
+  subscriptions = ['basic', 'advanced', 'pro']
+  defaultSubscription = 'advanced'
 
+  onSubmit() {
+    console.log('Form: ', this.signupForm.value)
+  }
 }
